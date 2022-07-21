0f2b7dfed3d22bbdabc47ca99ee98ef0eb72a197
Nick Chua <nickchua@Nicks-MBP-5.attlocal.net>
2022-07-21 14:56:10 -0700

    fixes

--- train/models_def/model_joint_all.py
+++ train/models_def/model_joint_all.py
@@ -154,7 +154,6 @@ class Model_Joint(nn.Module):
             elif self.model_type_light == "mobilenet_large":
                 self.LIGHT_Net.update({'lightEncoder':  models_mobilenet_light.MobileNetV3_Large_Light(opt)})
                 if 'axis' in opt.cfg.MODEL_LIGHT.enable_list:
-                    def __init__(self, opt, SGNum, mode=0, use_aspp=False, num_filters=128
                     self.LIGHT_Net.update({'axisDecoder':  models_mobilenet_light.LRASPP_Light(opt, SGNum = opt.cfg.MODEL_LIGHT.SGNum, mode=0 )})
                 if 'lamb' in opt.cfg.MODEL_LIGHT.enable_list:
                     self.LIGHT_Net.update({'lambDecoder':  models_mobilenet_light.LRASPP_Light(opt, SGNum = opt.cfg.MODEL_LIGHT.SGNum, mode=1 )})
